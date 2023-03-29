8360dc8a0bee09a188304ba7339f91026e3ef852
Robert <mirob1179@gmail.com>
2023-03-29 21:23:13 +0300

    First commit. Project used to learn about Spring transactions

---
+++ src/test/java/me/learnspring/springtransactions/SpringTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.learnspring.springtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
