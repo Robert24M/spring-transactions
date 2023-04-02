c8e2a181955c72b98d73637ae3426200a6639dbc
Robert <mirob1179@gmail.com>
2023-04-02 19:17:35 +0300

    Implement a test for TransferServiceSpringData for an exception thrown case

--- src/test/java/me/learnspring/springtransactions/services/TransferServiceSpringDataUnitTest.java
+++ src/test/java/me/learnspring/springtransactions/services/TransferServiceSpringDataUnitTest.java
@@ -1,25 +1,38 @@
 package me.learnspring.springtransactions.services;
 
+import me.learnspring.springtransactions.exceptions.AccountNotFoundException;
 import me.learnspring.springtransactions.model.Account;
 import me.learnspring.springtransactions.repositories.AccountRepositorySpringData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class TransferServiceSpringDataUnitTest {
+
+    @Mock
+    private AccountRepositorySpringData accountRepository;
+
+    @InjectMocks
+    private TransferServiceSpringData transferService;
 
     @DisplayName("Test the amount is transferred " +
             "from one account to another if no exception occurs.")
     @Test
     public void moneyTransferHappyFlow() {
-        AccountRepositorySpringData accountRepository = mock(AccountRepositorySpringData.class);
-        TransferServiceSpringData transferService = new TransferServiceSpringData(accountRepository);
 
         Account sender = new Account();
         sender.setId(1);
@@ -45,5 +58,24 @@ public class TransferServiceSpringDataUnitTest {
                 .changeBalance(1, new BigDecimal(900));
         verify(accountRepository)
                 .changeBalance(2, new BigDecimal(1100));
+    }
+
+    @Test
+    public void moneyTransferDestinationAccountNotFoundFlow() {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setBalance(new BigDecimal(1000));
+
+        given(accountRepository.findById(1L))
+                .willReturn(Optional.of(sender));
+        given(accountRepository.findById(2L))
+                .willReturn(Optional.empty());
+
+        assertThrows(
+                AccountNotFoundException.class,
+                () -> transferService.makeTransfer(1, 2, new BigDecimal(100)));
+
+        verify(accountRepository, never())
+                .changeBalance(anyLong(), any());
     }
 }
